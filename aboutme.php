f7e625bf64132cdfa40ebd52877f45af2544edb7
ramafikri02 <55729945+ramafikri02@users.noreply.github.com>
2020-04-08 15:37:46 +0700

    aboutme.php

---
+++ aboutme.php
@@ -0,0 +1,6 @@
+<?php 
+echo "Nama  : Muhammad Ramadhan Fikri </br>";
+echo "Kelas : XI - RPL 2 </br>";
+echo "Asal Sekolah : SMK Taruna Bhakti </br>";
+echo "Alamat Rumah : Perumahan Puspa Raya Blok EG No 22, rt/rw : 03/12, kecamatan/kelurahan : Bojong Gede/Bojong Baru, Kab.Bogor"
+?>
