05583b61cd0ce9f8deb605a441c554d286c46acf
ramafikri02 <55729945+ramafikri02@users.noreply.github.com>
2020-04-08 15:41:35 +0700

    add no telepon, email, delete alamat

--- aboutme.php
+++ aboutme.php
@@ -2,5 +2,6 @@
 echo "Nama  : Muhammad Ramadhan Fikri </br>";
 echo "Kelas : XI - RPL 2 </br>";
 echo "Asal Sekolah : SMK Taruna Bhakti </br>";
-echo "Alamat Rumah : Perumahan Puspa Raya Blok EG No 22, rt/rw : 03/12, kecamatan/kelurahan : Bojong Gede/Bojong Baru, Kab.Bogor"
+echo "No Telepon : 082113226020 </br>";
+echo "Alamat Email : ramafikri02@gmail.com </br>";
 ?>
